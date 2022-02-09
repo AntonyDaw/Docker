383e2cecf9fdcd91afbfca2e70500cb6e31e1a5a
Antony Luis Fernández Dearsley <antony.daw21@gmail.com>
2022-02-09 16:55:20 +0000

    Creating new index.php file

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello there</title>
+        <style>
+            .center {
+                display:flex;
+                justify-content:center;
+                width:100%;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+                echo "<h1>Nginx + PHP</h1>";
+        ?>
+    </body>
+</html>
